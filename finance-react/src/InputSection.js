573301928759b304bf66f32870e98db903e569c6
Jacob Korf <korfj7072@uwec.edu>
2020-05-17 12:45:33 -0500

    clean up purchases

--- finance-react/src/InputSection.js
+++ finance-react/src/InputSection.js
@@ -7,13 +7,6 @@ import { NewIncome } from './newIncome.js';
 import {useSelector, useDispatch} from 'react-redux';
 import {loadUserPurchases} from './actions';
 
-const initialPurchases = [
-{description: "Groceries", amount: "400"},
-{description: "Gas", amount: "10"},
-{description: "Car repair", amount: "$00"},
-{description: "Rent", amount: "350"},
-];
-
 const initialIncome = [
 {desc: "Weekly Deposit", value: "$400"},
 {desc: "lol", value: "$400"},
@@ -26,17 +19,8 @@ export function InputSection(props){
   const purchases = useSelector(state => state.purchases);
   const dispatch = useDispatch();
 
-    const onLoad = () => {
-      dispatch(loadUserPurchases(user))
-    }
-
-    function submitUsername(){
-      user = props.user;
-      onLoad.call();
-    }
+    
     const [usern, setUsern] = useState("");
-
-
 
     React.useEffect(() => { 
     user = props.user;
@@ -44,26 +28,7 @@ export function InputSection(props){
      })
 
     const [income, setIncome] = useState(initialIncome);
-   {/* const deletePurchase = name => {
-        setPurchases(purchases => purchases.filter(purchases => purchases.description !== name));
-    }
-    
-    const [purchases, setPurchases] = useState(initialPurchases);
-     user = props.user;
-  dispatch(loadUserPurchases(user));
-setPurchases(pur)
-
-React.useEffect(() => { alert("Your file is being uploaded!");
-user = props.user;
-dispatch(loadUserPurchases(user));
-setPurchases(pur);
- }, [userna])
-    const addPurchase = name => {
-        setPurchases(purchases => [name, ...purchases])
-    }
-    
-       <NewPurchase user = {props.user}/>*/
-    }
+  
     const deletePurchase = "";
 
     const deleteIncome = name => {
@@ -75,7 +40,7 @@ setPurchases(pur);
   return(
     <div id ="inputBox">
       <div id="incomeBox">
-        <p className="boxHeader">Incomes {props.user}</p>
+        <p className="boxHeader">Incomes</p>
         <NewIncome add = {addIncome}/>
         {income.map(inc => <Income key = {inc} inc = {inc} deleteIncome = {deleteIncome}/>)};
       </div>
