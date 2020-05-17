3a87551e4420bea51d42a13c82f1f4d2f8cf1c1f
Jacob Korf <korfj7072@uwec.edu>
2020-05-16 19:50:38 -0500

    moved OutputSection into own compenent Class

--- finance-react/src/App.js
+++ finance-react/src/App.js
@@ -2,33 +2,9 @@ import React from 'react';
 
 import './App.css';
 
-import CanvasJSReact from './canvasjs.react';
 import { InputSection } from './InputSection.js';
 import { HeaderFinance } from './HeaderFinance';
-//var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-const options = {
-  backgroundColor: "#282c34",
-  title: {
-    text: "Income Vs. Spending",
-    fontFamily: "Segoe UI",
-    fontColor: "white"
-   
-  },
-  axisY:{
-    labelFontColor: "white",
-  },
-  axisX:{
-    labelFontColor: "white",
-  },
-  data: [{				
-            type: "bar",
-            dataPoints: [
-                { label: "Spending", y: 478 , color: "red" },
-                { label: "Income",  y: 2700 , color: "green" },
-            ]
-   }]
-}
+import {OutputSection} from './OutputSection.js'
 
 function App(){
   return (
@@ -43,22 +19,5 @@ function App(){
 }
 
 
-
-function OutputSection(props){
- return (
-  <div id="outputBox">
-    <div id="spendingInfoBox">
-      <p className="boxHeader">Budgeting Information</p>
-      <p className="paragraphText">Percent of Income Spent: </p>
-      <p className="paragraphText">Percent of Income Saved: </p>
-    </div>
-    <div id="graphBox">
-      <div>
-        <CanvasJSChart options = {options} />
-      </div>
-    </div>
-  </div>
-  ); 
-}
 
 export default App;
