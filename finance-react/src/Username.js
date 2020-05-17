f48574c7be9bb7147e9db686720eedb21a4a3295
Jacob Korf <korfj7072@uwec.edu>
2020-05-16 19:47:58 -0500

    updated Username

--- finance-react/src/Username.js
+++ finance-react/src/Username.js
@@ -3,8 +3,8 @@ export function Username() {
     const [username, setUsername] = useState("");
     return (
         <div id = "userDiv">
-        <p className="usernameHeader">Username: <input type = "text" onChange = {e => setUsername(e.target.value)}/>
-        <input className = "addButton" type = "button"  value = "Log in" /> </p>
+        <p className="usernameHeader">Username: <input type = "text" onChange = {e => setUsername(e.target.value)}/></p>
+        <input className = "addButton" type = "button"  value = "Log in" /> 
         </div>
     );
 } 
